27c3c33d4077bbf89f2ba69c9985e44e5187ccc6
Decide Precise <decideprecise@gmail.com>
2021-08-16 19:49:58 +0530

    my first commit reactwesbsit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+// Bootstrap link here
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/css/bootstrap-grid.min.css'
+import '../node_modules/jquery/dist/jquery.min.js';
+import '../node_modules/popper.js/dist/popper.min.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+
+
